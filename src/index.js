ae4e0097f95630f5dc7143ce079ae6bab5d04641
AxelAngelucci <axeljesusangelucci@gmail.com>
2022-06-23 20:37:46 -0300

    fixed

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import express from "express";
 import morgan from "morgan";
 import router from "./routes/index.routes.js";
+import cors from "cors";
 const app = express();
 app.use(morgan("dev"));
+app.use(cors());
 app.use(router);
 
 app.listen("3001",()=>{
